ca6b776371fc71e52ef11f2c0857677188be3444
Armen Baghunts <gphbakarm@gmail.com>
2026-01-16 02:54:07 +0400

    Hometask-1 is committed

---
+++ Hometask-1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=5;
+        b=8;
+        c=4;
+        float k;
+        char ch='8';
+        double result=Math.pow(a+b,3);
+        k=(a+b+c)/3f-(a+b+c)/3;
+        System.out.println((a+b+c)/3f);
+        System.out.println(100*a+10*b+c);
+        System.out.println(k);
+        System.out.println((int) ch -48);
+        System.out.println(result);
+    }
+}
