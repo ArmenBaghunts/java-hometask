dd5562795898dab05c0b206bf1682a825af94f60
Armen Baghunts <gphbakarm@gmail.com>
2026-06-11 12:52:47 +0400

    The task about home appliances was committed

---
+++ homeAppliances/src/Address.java
@@ -0,0 +1,16 @@
+public class Address {
+    String country;
+    String city;
+    String street;
+
+    public Address(String country, String city, String street) {
+        this.country=country;
+        this.city=city;
+        this.street=street;
+    }
+
+    public  String toString(){
+        return country + ", " + city + ", " + street;
+    }
+}
+
